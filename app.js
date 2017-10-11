beb97b21aa2a1d5056f8b1a15bc58082e9448f3e
zut <ahilton@gmail.com>
2017-10-12 00:27:24 +1100

    bug fix

--- app.js
+++ app.js
@@ -8,6 +8,3 @@ var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, ()=>console.log('%s Listening to %s', server.name, server.url));
 
 server.post('/api/messages', bot.connector('*').listen());
-server.listen(process.env.PORT || 3978, () => {
-    console.log(`${server.name} listening to ${server.url}`);
-});
